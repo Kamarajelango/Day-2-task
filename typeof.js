f0cb875975ae0253a8fda912a4b942bc29b5bdb3
Kamarajelango <102349952+Kamarajelango@users.noreply.github.com>
2022-03-25 01:12:29 -0700

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,12 @@
+value=[1,1.1,'1.1',true,null,undefined,[],{},NaN]
+
+console.log(typeof(value[0]))
+console.log(typeof(value[1]))
+console.log(typeof(value[2]))
+console.log(typeof(value[3]))
+console.log(typeof(value[4]))
+console.log(typeof(value[5]))
+console.log(typeof(value[6]))
+console.log(typeof(value[7]))
+console.log(typeof(value[8]))
+
